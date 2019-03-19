1904b0321d072bc9d47f0c9125020d842ff01f53
Andreas Argelius <andreas@argeli.us>
2019-03-19 18:18:49 +0100

    Fix

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const LRU = require("lru-cache");
 
 const tokenCache = new LRU({
   max: 100,
-  maxAge: 1000 * 30
+  maxAge: 1000 * 60 * 2
 });
 
 function generateRandomToken() {
@@ -23,21 +23,24 @@ function generateRandomToken() {
   });
 }
 
-const USERS = [
+let users = [
   {
     name: "Bob",
     email: "bob@example.com",
-    password: "bob123"
+    password: "bob123",
+    age: 30
   },
   {
     name: "Alice",
-    email: "bob@example.com",
-    password: "alice123"
+    email: "alice@example.com",
+    password: "alice123",
+    age: 20
   },
   {
     name: "Eve",
     email: "eve@example.com",
-    password: "eve123"
+    password: "eve123",
+    age: 40
   }
 ];
 
@@ -46,6 +49,20 @@ const authSchema = Joi.object().keys({
     .email({ minDomainAtoms: 2 })
     .required(),
   password: Joi.string().required()
+});
+
+const profileSchema = Joi.object().keys({
+  name: Joi.string()
+    .min(1)
+    .max(40)
+    .required(),
+  email: Joi.string()
+    .email({ minDomainAtoms: 2 })
+    .required(),
+  age: Joi.number()
+    .integer()
+    .min(0)
+    .required()
 });
 
 const app = express();
@@ -89,7 +106,7 @@ app.post("/auth", async (req, res) => {
 
   const { email, password } = req.body;
 
-  const user = USERS.find(user => user.email === email);
+  const user = users.find(user => user.email === email);
 
   const loginSuccess = user && user.password === password;
 
@@ -135,7 +152,7 @@ app.get("/me", (req, res) => {
 
   const email = tokenCache.get(req.token);
 
-  const user = USERS.find(x => x.email === email);
+  const user = users.find(x => x.email === email);
 
   if (!user) {
     return res.status(401).json({
@@ -150,6 +167,60 @@ app.get("/me", (req, res) => {
     status: "success",
     profile: userWithoutPassword
   });
+});
+
+app.put("/me", (req, res) => {
+  if (!req.token) {
+    return res.status(401).json({
+      status: "error",
+      message: "token missing"
+    });
+  }
+
+  if (!tokenCache.has(req.token)) {
+    return res.status(401).json({
+      status: "error",
+      message: "token expired"
+    });
+  }
+
+  const email = tokenCache.get(req.token);
+
+  const user = users.find(x => x.email === email);
+
+  if (!user) {
+    return res.status(401).json({
+      status: "error",
+      message: "token invalid"
+    });
+  }
+
+  const result = Joi.validate(req.body, profileSchema);
+
+  if (result.error) {
+    return res.status(400).json({
+      status: "error",
+      message: "invalid parameters"
+    });
+  }
+
+  if (req.body.email !== user.email) {
+    return res.status(400).json({
+      status: "error",
+      message: "email cannot be changed"
+    });
+  }
+
+  const newUser = {
+    ...user,
+    ...req.body
+  };
+
+  const index = users.indexOf(user);
+
+  users = [...users.slice(0, index), newUser, ...users.slice(index + 1)];
+
+  return res.json(newUser);
 });
 
 app.listen(3000, () => console.log("Listening on port 3000"));
